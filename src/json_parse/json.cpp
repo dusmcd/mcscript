c8de39d3adec7f39e23ad75d2b20c8082504fdae
Dustin <dustinmcdowell1989@gmail.com>
2024-12-18 17:21:21 -0500

    splitting json string into meaningful components

--- src/json_parse/json.cpp
+++ src/json_parse/json.cpp
@@ -7,20 +7,65 @@ Json::Json(string json)
 
 Leaf* Json::parse()
 {
-
+    _components = _split_to_components(); 
 }
 
-string Json::_get_string(int& pos)
+vector<string> Json::_split_to_components()
+{
+    vector<string> components;
+    size_t length = _json.length();
+    for (size_t i = 0; i < length; i++)
+    {
+        char current = _json[i];
+        string current_str;
+        switch(current)
+        {
+            case ' ':
+                continue;
+            case '\n':
+                continue;
+            case '"':
+                components.push_back(_get_string(i));
+                break;
+            default:
+                if (isdigit(current))
+                {
+                    components.push_back(_get_integer(i));
+                }
+                else if (current == 't')
+                {
+                    components.push_back("true");
+                    i += 3;
+                }
+                else if (current == 'f')
+                {
+                    components.push_back("false");
+                    i += 4;
+                }
+                else
+                {
+                    current_str.insert(0, 1, current);
+                    components.push_back(current_str);
+                }
+        }
+    }
+    return components;
+}
+
+string Json::_get_string(size_t& pos)
 {
     string result;
     int i = 0;
     while (true)
     {
-        char current = _json[pos++];
+        pos++;
+        char current = _json[pos];
         if (current == '"')
+        {
             return result;
+        }
         
-        result.insert(i, 0, current);
+        result.insert(i, 1, current);
         i++;
     }
 }
@@ -30,3 +75,25 @@ void Json::set_json(string json)
     // validate json later
     _json = json;
 }
+
+string Json::_get_integer(size_t& pos)
+{
+    string result;
+    char current;
+    int i = 0;
+    while (true)
+    {
+        current = _json[pos];
+        if (pos + 1 >= _json.length() || !isdigit(_json[pos + 1]))
+        {
+            result.insert(i, 1, current);
+            return result;
+        }
+
+        result.insert(i, 1, current);
+        i++;
+        pos++;
+    }
+
+}
+
--- src/json_parse/json.h
+++ src/json_parse/json.h
@@ -3,8 +3,19 @@
 
 #include <string>
 #include "../types.h"
+#include <vector>
 
 using std::string;
+using std::vector;
+
+/*
+    {
+        "key": "value",
+        "key2": 2,
+        "key3": "hello there" 
+    }
+    [{, "key", ":", "value", ",", "key2", 2, ","]
+*/
 
 class Json
 {
@@ -16,8 +27,11 @@ class Json
         void set_json(string json);
 
     private:
-        string _get_string(int& pos);
+        string _get_string(size_t& pos);
+        string _get_integer(size_t& pos);
         string _json;
+        vector<string> _split_to_components();
+        vector<string> _components;
 };
 
 
