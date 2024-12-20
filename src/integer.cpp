35895f71164f2d2e91a1ce0e28c4f832520449a2
Dustin <dustinmcdowell1989@gmail.com>
2024-12-20 13:16:40 -0500

    integer class

---
+++ src/integer.cpp
@@ -0,0 +1,25 @@
+#include "integer.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+Integer::Integer(int num)
+{
+   set_num(num); 
+}
+
+void Integer::Print()
+{
+    cout << _num << endl;
+}
+
+void Integer::set_num(int num)
+{
+    _num = num;
+}
+
+int Integer::get_num() const
+{
+    return _num;
+}
---
+++ src/integer.h
@@ -0,0 +1,24 @@
+#ifndef _INTEGER_H
+#define _INTEGER_H
+
+#include "object.h"
+
+class Integer : public Object
+{
+
+    public:
+        Integer(int num);
+        void Print();
+
+        // setters
+        void set_num(int num);
+
+        // getters
+        int get_num() const;
+
+    private:
+        int _num;
+};
+
+
+#endif
